2f5b614c59b198270e17591b13ef035be1cde805
doanb <doanburak97@gmail.com>
2021-02-27 21:55:43 +0300

    fourth commit

--- app/src/main/java/com/doanburak/instagramapp/MainPageActivity.java
+++ app/src/main/java/com/doanburak/instagramapp/MainPageActivity.java
@@ -9,17 +9,27 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.Map;
 
 public class MainPageActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
     private FirebaseFirestore firebaseFirestore;
+    ArrayList<String> emailFromFB;
+    ArrayList<String> commentFromFB;
+    ArrayList<String> urlFromFB;
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -51,6 +61,10 @@ public class MainPageActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
 
+        emailFromFB = new ArrayList<>();
+        commentFromFB = new ArrayList<>();
+        urlFromFB = new ArrayList<>();
+
         mAuth = FirebaseAuth.getInstance();
         firebaseFirestore = FirebaseFirestore.getInstance();
 
@@ -59,10 +73,32 @@ public class MainPageActivity extends AppCompatActivity {
 
     public void getDataFromFirestore(){
 
-        firebaseFirestore.collection("Posts").addSnapshotListener(new EventListener<QuerySnapshot>() {
+        CollectionReference collectionReference = firebaseFirestore.collection("Posts");
+        collectionReference.orderBy("date", Query.Direction.DESCENDING).addSnapshotListener(new EventListener<QuerySnapshot>() {
             @Override
             public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
 
+                if (error != null){
+                    Toast.makeText(MainPageActivity.this, error.getLocalizedMessage().toString(), Toast.LENGTH_SHORT).show();
+                }
+
+                if (value != null){
+
+                    for (DocumentSnapshot snapshot : value.getDocuments()){
+                        Map<String, Object> data = snapshot.getData();
+
+                        //Casting
+                        String comment = (String) data.get("comment");
+                        String email = (String) data.get("email");
+                        String downloadUrl = (String) data.get("downloadUrl");
+
+                        emailFromFB.add(email);
+                        commentFromFB.add(comment);
+                        urlFromFB.add(downloadUrl);
+
+                    }
+
+                }
 
 
             }
